f7efb60d590089e9ce47ccf012b7787d77e268a7
yuri.andrade <yuri.andarde@dbserver.com.br>
2022-07-05 16:43:57 -0300

    variaveis mutável(var) e imutável(val).

--- src/main/kotlin/Variaveis.kt
+++ src/main/kotlin/Variaveis.kt
@@ -1,5 +1,5 @@
 fun main() {
-    var nome = "Yuri" // valor pode ser alterado
+    var nome: String = "Yuri" // valor pode ser alterado
 
-    val variavelImutavel = "Yuri" // val é como se fosse a const no javascript, o valor não pode ser alterado
+    val variavelImutavel: String = "Yuri" // val é como se fosse a const no javascript, o valor não pode ser alterado
 }
