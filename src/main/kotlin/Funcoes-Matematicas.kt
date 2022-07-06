cfab2d312bccd0ad7861eeca4f4c07eb42dd2476
yuri.andrade <yuri.andarde@dbserver.com.br>
2022-07-06 06:46:24 -0300

    funções matematicas

---
+++ src/main/kotlin/Funcoes-Matematicas.kt
@@ -0,0 +1,13 @@
+import kotlin.math.*
+
+fun main() {
+
+    println(max(5, 10))
+    println(min(5, 10))
+    println(sqrt(45.67f))
+    println(PI)
+    println(E)
+    println(round(35465.32323232345))
+    println()
+
+}
