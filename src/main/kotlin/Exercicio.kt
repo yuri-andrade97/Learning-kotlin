b8769fa9cc07e29a8a3a9dbe6aadd0ece107340b
yuri.andrade <yuri.andarde@dbserver.com.br>
2022-07-06 05:46:41 -0300

    exercicios session5

---
+++ src/main/kotlin/Exercicio.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var nomeCompleto: String = "Erlhiton Yuri Santos Andrade"
+
+    val variavelSemTexto: String
+
+    val menorTipoDeDados: Byte = 40
+
+    val pibBrasil: Long = 1869000000000
+
+    val populacaoBrasil: Int = 211000000
+
+    println("O pib percapita do Brasil é R$ ${pibBrasil / populacaoBrasil}" )
+
+}
